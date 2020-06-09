5904dc22710ec0ec6309cef45da4d3d15eae000d
ilacilac <qkralswl307@gmail.com>
2020-06-09 17:58:19 +0900

    feat: minji's JS with localStorage

--- script/min.js
+++ script/min.js
@@ -52,36 +52,36 @@ let todos = [
   // Function
   // ----- render todos
   const render = () => {
-
-    let html = '';
-    todos.forEach((todo) => {
-      html += `
-      <li class="todo" id="${todo.id}">
-        <div class="todoExplain">
-          <h3 class="todoTitle">${todo.todoTitle}</h3>
-          <p class="todoCompleted">${todo.todoCompleted}</p>
-          <p class="todoImportance">${todo.todoImportance}</p>
-          <p class="todoDeadLine">${todo.todoDeadLine}</p>
-          <p class="todoKeyword">${todo.todoKeyword}</p>
-        </div>
-        <div class="hidden toggleTodo" id="toggleTodo">
-          <p class="todoContents">${todo.todoContents}</p>
-          <button class="fixBtn icon-pencil"></button>
-          <button class="delBtn icon-trash-empty"></button>
-          <button class="completeBtn icon-ok"></button>
-        </div>
-      </li>`;
-    });
-    // console.log(todos);
-    $todos.innerHTML = html;
-
-    console.log('render', todos);
+    const todosKey = Object.keys(localStorage) // 로컬 스토리지 키들로 이루어진 배열
+    todosKey.forEach( key => {
+      let todosObj = JSON.parse(localStorage.getItem(key));
+      let html = '';
+        html += `
+        <li class="todo" id="${todosObj.id}">
+          <div class="todoExplain">
+            <h3 class="todoTitle">${todosObj.todoTitle}</h3>
+            <p class="todoCompleted">${todosObj.todoCompleted}</p>
+            <p class="todoImportance">${todosObj.todoImportance}</p>
+            <p class="todoDeadLine">${todosObj.todoDeadLine}</p>
+            <p class="todoKeyword">${todosObj.todoKeyword}</p>
+          </div>
+          <div class="hidden toggleTodo" id="toggleTodo">
+            <p class="todoContents">${todosObj.todoContents}</p>
+            <button class="fixBtn icon-pencil"></button>
+            <button class="delBtn icon-trash-empty"></button>
+            <button class="completeBtn icon-ok"></button>
+          </div>
+        </li>`;
+      // console.log(todos);
+      $todos.innerHTML += html;
+    })
   };
 
 
   // ----- Add todo
   const addTodo = () => {
-    todos = [...todos, {
+    let addTodoObject;
+    addTodoObject = {
       id: genterateId(),
       todoTitle: $addTodoTitle.value,
       todoCompleted: false,
@@ -89,13 +89,23 @@ let todos = [
       todoDeadLine: `${addTodoYear.value}/${addTodoMonth.value}/${addTodoDay.value}/${addTodoHour.value}:${addTodoMinute.value}`,
       todoKeyword: addTodoKeywords(),
       todoContents: $addTodoContents.value,
-    }];
+    }
+    localStorage.setItem(`${genterateId()}`,JSON.stringify(addTodoObject));
+    
+    
     // keywords = '';
     if ($addTodoTitle.value === '') {
-      console.log('입력하세용');
-      // return false;
+      $addTodoTitle.focus();
+      $addTodoTitle.setAttribute('placeholder', '!!!!!빈칸없이 입력해주세요.');
+      return false;
+    } else if ($addTodoContents.value === '') {
+      $addTodoContents.focus();
+      $addTodoContents.setAttribute('placeholder', '!!!!!빈칸없이 입력해주세요.');
+      return false;
     }
-    console.log('ok');
+    
+    $addTodoSectionForm.reset();
+    importance = 0;
 
     render();
   }; 
@@ -103,11 +113,6 @@ let todos = [
   // ----- Generate Id
   const genterateId = () => {
     return (todos.length) ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
-  };
-
-  // ----- Add Keyword 
-  const getTodoImportance = id => {
-    return importance = id;
   };
 
   // ----- Check Todo Keyword
@@ -120,7 +125,6 @@ let todos = [
       keywordsHtml += `<button class="checkTodoKeyword">${addkeyword}</button>`
     });
     $checkTodoKeywords.innerHTML = keywordsHtml;
-    // addKeywords = addKeywords;
   };
 
   // ----- Add Todo Keywords
@@ -128,30 +132,31 @@ let todos = [
     return keywords.join(' ');
   };
 
-  // ----- todoKeyword
-
   // Event Handler
   window.onload = render();
 
   // ----- Add Keyword 
-  /* 
-    1. input에 키워드를 입력한다.
-    2. 키워드를 입력후 엔터를 누르면 추가된다.
-    3. 3개가 넘어가면 return한다.
-    4. 키워드를 누르면 삭제한다.
-  */
-
-  // li의 id와, todo의 id 비교후 맞으면 todo의 keyword에 push 해주기
   $addTodoKeyword.onkeyup = e => {
     if (e.keyCode !== 13) return;
     checkTodoKeyword(`#${$addTodoKeyword.value}`);
     $addTodoKeyword.value = '';
   };
 
+  // ----- Click Add Button
   $addTodoBtn.onclick = () => {
     addTodo();
-    $addTodoSectionForm.reset();
-    importance = 0;
+    $addTodoTitle.setAttribute('placeholder', '제목을 입력해주세요(7자 이내)');
+    $addTodoContents.setAttribute('placeholder', '할 일을 설명해주세요!');
+    [...$addTodoImportance.children].forEach((button) => {
+        button.innerHTML = '☆';
+    });
+    $checkTodoKeywords.innerHTML = '';
+    $closePopupBtn.click();
+  };
+
+  // ---- Click Keyword in popup
+  $checkTodoKeywords.onclick = (e) => {
+    // e.target.par
   };
 
   // ----- importance
@@ -165,5 +170,9 @@ let todos = [
       }
     });
     importance = +e.target.id;
+  };
+
+  $closePopupBtn.onclick = (e) => {
+    $addTodoSection.classList.add('hidden');
   };
 })();
