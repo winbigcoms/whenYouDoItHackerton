4b52f89fdac6a5be6d1a83cde1a9dc5e47d78fab
nomadhailey <blessedgene11@gmail.com>
2020-06-09 15:59:22 +0900

    todos 삭제 이벤트

--- script/hea.js
+++ script/hea.js
@@ -1,12 +1,12 @@
 // 추가 버튼 클릭 시 폼 띄우기
 const $openAddTab = document.querySelector('.openAddTab');
-const $addTodoSection = document.querySelector('.addTodoSection');
+// const $addTodoSection = document.querySelector('.addTodoSection');
 
 $openAddTab.onclick = () => {
     $addTodoSection.classList.toggle('hidden');
   };
 // 할 일(li) 클릭 시 창 아래로 길게 펼쳐지는 것
-const $todos = document.querySelector('.todos');
+// const $todos = document.querySelector('.todos');
 const $toggleTodo = document.getElementById('toggleTodo');
 $todos.onclick = ({ target }) => {
   if (!target.matches('.todo') && !target.matches('.todoExplain') && !target.matches('.todoTitle') && !target.matches('p')) return;
@@ -18,7 +18,25 @@ $todos.onclick = ({ target }) => {
   } else {
     target.parentNode.nextElementSibling.classList.toggle('hidden');
   }
-  
-   console.log(target);
 };
 
+// todos 수정
+// const $fixBtn = document.querySelector('.fixBtn');
+// $fixBtn.onclick = () => {
+//   $addTodoSection.classList.toggle('hidden'); 
+// };
+// todos 삭제
+const delBtn = id => {
+  todos = todos.filter(todo => todo.id !== +id);
+};
+$delBtn.onclick = ({ target }) => {
+  // if(!target.matches('.toggleTodo > .delBtn')) return;
+  delBtn(target.parentNode.parentNode.id);
+  console.log(target.parentNode.parentNode.id)
+  render();
+};
+// todos 완료체크
+// const $completeBtn = document.querySelector('.completeBtn');
+// $completeBtn.onclick = () => {
+
+// };
